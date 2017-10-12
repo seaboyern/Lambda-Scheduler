6f3e9f78db82e96a448912f8b4e4babd9e736096
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-10-12 16:38:03 -0600

    Added dynamic adding and deletion of events in weekly view

--- app/src/main/java/com/example/lambda/lambdaorganizer/ScheduleOverview.java
+++ app/src/main/java/com/example/lambda/lambdaorganizer/ScheduleOverview.java
@@ -25,18 +25,22 @@ import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidListener;
 
 public class ScheduleOverview extends AppCompatActivity implements WeekView.EventClickListener,
-        MonthLoader.MonthChangeListener, WeekView.EmptyViewClickListener {
+        MonthLoader.MonthChangeListener, WeekView.EmptyViewClickListener,
+        WeekView.EventLongPressListener{
 
     private static final int TYPE_DAY_VIEW = 1;
     private static final int TYPE_THREE_DAY_VIEW = 2;
     private static final int TYPE_WEEK_VIEW = 3;
     private int mWeekViewType = TYPE_THREE_DAY_VIEW;
     private WeekView mWeekView;
+    private List<WeekViewEvent> mEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_overview);
+
+        mEvents = new ArrayList<WeekViewEvent>();
 
         //////////////////////
         // Set up Week view //
@@ -52,6 +56,7 @@ public class ScheduleOverview extends AppCompatActivity implements WeekView.Even
         // Handle events generated when user clicks on events and empty space
         mWeekView.setOnEventClickListener(this);
         mWeekView.setEmptyViewClickListener(this);
+        mWeekView.setEventLongPressListener(this);
 
         ///////////////////////
         // Set up Month View //
@@ -130,12 +135,25 @@ public class ScheduleOverview extends AppCompatActivity implements WeekView.Even
     }
 
     /**
+     * Callback when user long presses an event on the weekly view.
+     * @param event
+     * @param eventRect
+     */
+    @Override
+    public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+        deleteTask(event);
+    }
+
+    /**
      * Callback when user touches an empty space on the weekly view.
      * @param time: The time corresponding to the empty space.
      */
     @Override
     public void onEmptyViewClicked(Calendar time) {
-        addTask("Test", time);
+        Calendar endTime = (Calendar) time.clone();
+        endTime.add(Calendar.HOUR, 1);
+
+        addTask("Test", time, endTime);
     }
 
     /**
@@ -152,30 +170,24 @@ public class ScheduleOverview extends AppCompatActivity implements WeekView.Even
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
         // Populate the week view with some events.
-        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
 
-        Calendar startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth - 1);
-        startTime.set(Calendar.YEAR, newYear);
-        Calendar endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR, 1);
-        endTime.set(Calendar.MONTH, newMonth - 1);
-        WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_01));
-        events.add(event);
-
-        return events;
+        return mEvents;
     }
 
     /**
      * Add a new task to the weekly view
      * @param title
-     * @param time
+     * @param startTime
+     * @param endTime
      */
-    public void addTask(String title, Calendar time) {
-        Toast.makeText(this, "TODO: Create new event.", Toast.LENGTH_SHORT).show();
+    public void addTask(String title, Calendar startTime, Calendar endTime) {
+        WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
+        event.setColor(getResources().getColor(R.color.event_color_01));
+
+        mEvents.add(event);
+
+        // refresh week view. Not good if events added to a day other than today
+        mWeekView.goToToday();
     }
 
     /**
@@ -184,5 +196,11 @@ public class ScheduleOverview extends AppCompatActivity implements WeekView.Even
      */
     public void showTask(WeekViewEvent event) {
         Toast.makeText(this, "TODO: Open event " + event.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void deleteTask(WeekViewEvent event) {
+        Toast.makeText(this, "TODO: remove event " + event.getName(), Toast.LENGTH_SHORT).show();
+        mEvents.remove(mEvents.indexOf(event));
+        mWeekView.goToToday();
     }
 }
