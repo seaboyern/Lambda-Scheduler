b446de8f9f2e100466df5cf84aa20fce03a3f7e3
priomkhan <31937001+priomkhan@users.noreply.github.com>
2017-10-13 00:53:34 -0600

    Google Calendar API Added with updated code. XML fixed. Buttons eg "Add Study" and "Back to Manu" added.

--- app/src/main/java/com/example/lambda/lambdaorganizer/MainActivity.java
+++ app/src/main/java/com/example/lambda/lambdaorganizer/MainActivity.java
@@ -1,56 +1,34 @@
 package com.example.lambda.lambdaorganizer;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.LinearLayout;
 
-//public class MainActivity extends Activity implements EasyPermissions.PermissionCallbacks {
-public class MainActivity extends AppCompatActivity  {
-    private Button mCallApiButton;
-    GoogleCalAPI newCalender;
-    private static final String BUTTON_TEXT = "Call Google Calendar API";
+public class MainActivity extends AppCompatActivity {
 
 
-    /**
-     * Create the main activity.
-     * @param savedInstanceState previously saved instance data.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout activityLayout = new LinearLayout(this);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT);
-        activityLayout.setLayoutParams(lp);
-        activityLayout.setOrientation(LinearLayout.VERTICAL);
-        activityLayout.setPadding(16, 16, 16, 16);
+        setContentView(R.layout.activity_main);
 
-        mCallApiButton = new Button(this);
-        mCallApiButton.setText(BUTTON_TEXT);
+        // declaration of all objects
 
-        newCalender = new GoogleCalAPI(this);
+        final Button btnSessionEng = (Button) findViewById(R.id.launch_sessioneng_btn);
 
-        mCallApiButton.setOnClickListener(new View.OnClickListener() {
+
+        // button initialization
+
+
+        btnSessionEng.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCallApiButton.setEnabled(false);
-
-                newCalender.getmOutputText().setText("");
-                newCalender.getResultsFromApi();
-                mCallApiButton.setEnabled(true);
+                startActivity(new Intent(MainActivity.this, SessionEngineer.class));
             }
         });
-        activityLayout.addView(mCallApiButton);
 
-        activityLayout.addView(newCalender.getmOutputText());
-
-        setContentView(activityLayout);
 
     }
-
 }
