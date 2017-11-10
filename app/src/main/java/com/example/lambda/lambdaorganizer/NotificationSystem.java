76e3e70fd8e27b9231941d0c716a42f9f3c401c6
jjball <jadenjball@gmail.com>
2017-11-09 18:26:24 -0600

    Procedure for creating notifications and cancelling notifications

--- app/src/main/java/com/example/lambda/lambdaorganizer/NotificationSystem.java
+++ app/src/main/java/com/example/lambda/lambdaorganizer/NotificationSystem.java
@@ -3,11 +3,17 @@ package com.example.lambda.lambdaorganizer;
 
 import android.app.Notification;
 //import android.support.v4.app.TaskStackBuilder;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 //import android.support.v7.app.NotificationCompat;
 import android.app.NotificationManager;
 //import android.app.PendingIntent;
 import android.content.Context;
+
+import java.util.List;
 //import android.content.Intent;
 
 
@@ -16,6 +22,8 @@ import android.content.Context;
  */
 
 public class NotificationSystem {
+
+    private static List<Integer> notifIDs;
 
     public static void sendNotification(AppCompatActivity activity, String subject, String body){
 
@@ -32,38 +40,45 @@ public class NotificationSystem {
 
     }
 
-//    public static void notifInit(){
-        //        NotificationCompat.Builder mBuilder =
-//                new NotificationCompat.Builder(this)
-//                        .setSmallIcon(R.drawable.lambda_icon)
-//                        .setContentTitle("Lambda Organizer")
-//                        .setContentText("Lambda Organizer notification");
-//        // Creates an explicit intent for an Activity in your app
-//        Intent resultIntent = new Intent(this, ResultActivity.class);
-//
-//        // The stack builder object will contain an artificial back stack for the
-//        // started Activity.
-//        // This ensures that navigating backward from the Activity leads out of
-//        // your app to the Home screen.
-//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-//        // Adds the back stack for the Intent (but not the Intent itself)
-//        stackBuilder.addParentStack(ResultActivity.class);
-//        // Adds the Intent that starts the Activity to the top of the stack
-//        stackBuilder.addNextIntent(resultIntent);
-//        PendingIntent resultPendingIntent =
-//                stackBuilder.getPendingIntent(
-//                        0,
-//                        PendingIntent.FLAG_UPDATE_CURRENT
-//                );
-//        mBuilder.setContentIntent(resultPendingIntent);
-//        NotificationManager mNotificationManager =
-//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//        // mNotificationId is a unique integer your app uses to identify the
-//        // notification. For example, to cancel the notification, you can pass its ID
-//        // number to NotificationManager.cancel().
-//        int mNotificationId = 1; // temporary until system for giving unique ids is made
-//        mNotificationManager.notify(mNotificationId, mBuilder.build());
-//    }
+    public static void notifCreate(AppCompatActivity activity, AppCompatActivity resultActivity, int notifID, String msg){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(activity)
+                        .setSmallIcon(R.drawable.lambda_icon)
+                        .setContentTitle("Lambda Organizer")
+                        .setContentText(msg);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(activity, resultActivity.getClass()); /*this, ResultActivity.class*/
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your app to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(activity);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(resultActivity);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // mNotificationId is a unique integer your app uses to identify the
+        // notification. For example, to cancel the notification, you can pass its ID
+        // number to NotificationManager.cancel().
+        mNotificationManager.notify(notifID, mBuilder.build());
+    }
+
+
+    // Cancels the notification with the id notifID
+    public static void notifCancel(AppCompatActivity activity, int notifID){
+        NotificationManager mNotificationManager =
+                (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(notifID);
+    }
 
 }
