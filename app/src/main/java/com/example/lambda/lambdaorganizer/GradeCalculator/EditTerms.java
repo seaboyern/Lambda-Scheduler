95ade0cbd97316c181105b338b085f70ec2420c3
Nicholas Seaboyer (nas260) <nap.s@usask.ca>
2017-11-19 21:42:34 -0600

    Adds functionality to Edit terms view.

--- app/src/main/java/com/example/lambda/lambdaorganizer/GradeCalculator/EditTerms.java
+++ app/src/main/java/com/example/lambda/lambdaorganizer/GradeCalculator/EditTerms.java
@@ -4,15 +4,103 @@ package com.example.lambda.lambdaorganizer.GradeCalculator;
  * Created by Nicholas on 2017-10-15.
  */
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.lambda.lambdaorganizer.R;
 
-public class EditTerms extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import GradeCalculatorObjects.Term;
+
+import static com.example.lambda.lambdaorganizer.R.id.Submit_terms;
+import static com.example.lambda.lambdaorganizer.R.id.editText;
+
+
+public class EditTerms extends Activity {
+
+    private Spinner spinnerterm;
+    private Button submitBtn;
+    protected final LinkedList<Term> terms = new LinkedList<>();
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.grade_calc_edit_terms); // <-- change view when ready
+        setContentView(R.layout.grade_calc_edit_terms);
+
+        addSpinnerItems();
+        addListenerOnBtn();
+
     }
+
+    public void addSpinnerItems(){
+
+        spinnerterm = (Spinner)findViewById(R.id.spinner3);
+
+        List<String> spinnerItems = new ArrayList<>();
+        spinnerItems.add("<Please Select Term>");
+
+        for(int i = 0; i < terms.size() - 1; i++){
+            spinnerItems.add(terms.get(i).getName());
+        }
+
+        spinnerItems.add("<New Term>");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, spinnerItems);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerterm.setAdapter(dataAdapter);
+
+        dataAdapter.notifyDataSetChanged();
+
+    }
+
+    public void addListenerOnBtn(){
+
+        submitBtn = (Button) findViewById(Submit_terms);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String valueChosen = String.valueOf(spinnerterm);
+
+                if(valueChosen.compareTo("<New Term>") == 0){
+                    Term newTerm = new Term(findViewById(editText).toString());
+                    terms.add(newTerm);
+
+                }
+                else if(valueChosen.compareTo("<Please Select Term>") == 0){
+                    toast("Select a Term");
+                }
+                else if(findViewById(editText).toString().compareTo("") == 0){
+                    toast("Input name");
+                }
+            }
+        });}
+
+    public void toast(final String toastStr) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(EditTerms.this, toastStr, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+
+
 }
