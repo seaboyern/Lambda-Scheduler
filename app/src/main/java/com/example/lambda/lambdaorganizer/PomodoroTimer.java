ddcf3d18d6a4c6c4d942913a57c05caaea08e6b4
Larry <larryabim@gmail.com>
2017-11-21 18:19:44 -0600

    Final changes made to timer

--- app/src/main/java/com/example/lambda/lambdaorganizer/PomodoroTimer.java
+++ app/src/main/java/com/example/lambda/lambdaorganizer/PomodoroTimer.java
@@ -1,6 +1,7 @@
 package com.example.lambda.lambdaorganizer;
 
 
+import android.app.Notification;
 import android.icu.util.TimeUnit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.example.lambda.lambdaorganizer.NotificationSystem.NotificationSystem;
 
 import java.sql.Time;
 
@@ -25,6 +28,7 @@ private boolean boolp = false;
     Button stop;
     Button pause;
     Button resume;
+    AppCompatActivity pomoActivity = this;
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -43,24 +47,41 @@ private boolean boolp = false;
          resume = (Button) findViewById(R.id.button13);
         text = (TextView) findViewById(R.id.Time);
 
+        /* Settings for the buttons
+
+         */
+
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
+                    /* Boolean values to check for when to cancel */
                 boolp = false;
                 boolc = false;
-                int minutes = 15000000;
+                int minutes = 1500000;
+                /*
+               // Testing methods
                 int min = 10000;
-                CountDownTimer timer = new CountDownTimer(min,1000) {
+
+                */
+                CountDownTimer timer = new CountDownTimer(minutes,1000) {
                     @Override
+
+                    /*
+                    Stop if its true
+                     */
                     public void onTick(long l) {
                         if(boolc || boolp){
                             cancel();
                         }
                                 else {
+
+                            /* Setting it to minutes and seconds format
+
+                             */
                             String format = String.format("%02d:%02d", java.util.concurrent.TimeUnit.MILLISECONDS.toMinutes(l),
-                                    java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(l));
+                                    java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(l)-
+                                    java.util.concurrent.TimeUnit.MINUTES.toSeconds(java.util.concurrent.TimeUnit.MILLISECONDS.toMinutes(l)));
                             text.setText(format);
                             timeRemaing = l;
                         }
@@ -68,7 +89,11 @@ private boolean boolp = false;
 
                     @Override
                     public void onFinish() {
+                        /*
+                        On finish set it back to 25 minutes and is done
+                         */
                         text.setText("25:00");
+                        NotificationSystem.sendNotification(pomoActivity,pomoActivity,1,"Time Done");
 
 
 
@@ -77,6 +102,10 @@ private boolean boolp = false;
 
             }
         });
+
+        /* Setting the stop timer
+
+         */
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,21 +113,35 @@ private boolean boolp = false;
                 text.setText("25:00");
             }
         });
+        /* Setting the resume timer
+
+         */
         resume.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolp =false;
                 boolc = false;
+                /* Time remaining for the app
+
+                 */
                 long newTime = timeRemaing;
                 CountDownTimer timer = new CountDownTimer(newTime,1000) {
                     @Override
+                    /*
+                        If this is true it cancels on tick
+                     */
                     public void onTick(long l) {
                         if(boolp || boolc){
                             cancel();
                         }
+
                         else{
+                            /*
+                            Setting the text and formatting if its done
+                             */
                             String format = String.format("%02d:%02d", java.util.concurrent.TimeUnit.MILLISECONDS.toMinutes(l),
-                                    java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(l));
+                                    java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(l)-
+                                            java.util.concurrent.TimeUnit.MINUTES.toSeconds(java.util.concurrent.TimeUnit.MILLISECONDS.toMinutes(l)));
                             text.setText(format);
                             timeRemaing = l;
                         }
@@ -112,6 +155,10 @@ private boolean boolp = false;
 
             }
         });
+
+        /*
+        Setting the boolp variable to true
+         */
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
